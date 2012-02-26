bed82d3cc0b47caacf9db92b21b71a7f63723517
Konstantin Borisov <smeagol74@gmail.com>
2012-02-26 22:32:28 +0700

    Добавил выгрузку протоколов для пакетной обработки на сайтах партнёрах

--- config/routes.rb
+++ config/routes.rb
@@ -96,4 +96,12 @@ Nkapp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  namespace :exchange do
+    get 'index'
+    post 'export'
+    post 'export_confirmed'
+    post 'import'
+  end
+
 end
