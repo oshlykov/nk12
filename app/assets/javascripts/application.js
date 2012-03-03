20a358d532d473626d3a2226ef5afb4a6e344ee4
Олег Шлыков <oshlykov@iteq.ru>
2012-03-03 11:02:02 +0400

    Перестановка приоритетов скриптов загрузки

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require_tree ../../../vendor/assets/javascripts/.
 //= twitter/bootstrap
 //= require_tree .
-//= require_tree ../../../vendor/assets/javascripts/.
