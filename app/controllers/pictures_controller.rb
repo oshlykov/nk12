c53908049d631b0348f7a4704254c9004540a16e
Олег Шлыков <oshlykov@iteq.ru>
2012-03-03 10:50:49 +0400

    Добавлена интеграция с мобильным приложением КАРИК

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,18 +1,38 @@
 class PicturesController < InheritedResources::Base
   belongs_to :protocol, :folder, :polymorphic => true, :optional => true
 
-  before_filter :auth, :except => [:index, :show]
+  before_filter :auth, :except => [:index, :show, :create]
 
   def create
-    if params.include? :login and params.include? :password
+    if params.include? 'login' and params.include? 'password'
       #mobile upload
-        if user = User.find_by_email(params[:email]) and user.authenticate(params[:password])
-          
-          # file = file.with_indifferent_access if file.is_a?(Hash) 
-          # if file.is_a?(Hash) && file.has_key?(:filename) && 
-          #   file.has_key?(:content_type) && file.has_key?(:data) && !file.has_key?(:tempfile) 
-          # file[:tempfile] = StringIO.new(Base64.decode64(file.delete(:data)))       
+      if user = User.find_by_email(params['login'])
+        unless user.authenticate(params['password'])
+          render(:file => "public/403.html", :status => '403') and return 
         end
+
+        params['picture'] = {"image" => params['file']}
+
+        if user.commission and user.commission.is_uik
+          if p = user.commission.protocols.where(:user_id => user.id).first
+            params['protocol_id'] = p.id
+          else
+            p = user.commission.protocols.new
+            p.user = user
+            p.priority = user.commission.protocols.size + 100
+            p.save
+            params['protocol_id'] = p.id
+          end
+          params['commission_id'] = user.commission_id
+        else
+          render :file => "public/404.html", :status => '404' and return
+        end
+
+        # file = file.with_indifferent_access if file.is_a?(Hash) 
+        # if file.is_a?(Hash) && file.has_key?(:filename) && 
+        #   file.has_key?(:content_type) && file.has_key?(:data) && !file.has_key?(:tempfile) 
+        # file[:tempfile] = StringIO.new(Base64.decode64(file.delete(:data)))       
+      end
 =begin
     print_r($_FILES);
     $heades_printed = print_r($_SERVER, true);
