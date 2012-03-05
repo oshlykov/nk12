75971b5f9e7315740545ce1352cb0642c8a0118e
kimae <kimae@sept2000.ru>
2012-03-05 22:45:07 +0300

    * circumvent fileupload index bug

--- app/assets/javascripts/folders.js
+++ app/assets/javascripts/folders.js
@@ -17,8 +17,8 @@ $(document).ready(function(){
     $('.pics_upload').fileUploadUI({
       uploadTable: $('.upload_files'),
       buildUploadRow: function (files, index) {
-        alert(index);
-        return $('<tr><td>' + files[index].name + '<\/td>' +
+        i = $('.pics_upload').data('filecount');
+        return $('<tr><td>' + files[i++].name + '<\/td>' +
 	  '<td class="file_upload_progress"><div><\/div><\/td>' +
 	  '<td class="file_upload_cancel">' +
 	  '<button class="ui-state-default ui-corner-all" title="Cancel">' +
@@ -29,7 +29,10 @@ $(document).ready(function(){
       buildDownloadRow: function (file) {
         return $('<div id="picture_'+file.id+'" class="picture">'+
 	  '<a href="'+file.url+'"><img src="'+file.preview_url+'"/></a>'+
-	  '<a rel="nofollow" data-remote="true" data-method="delete" data-confirm="Удалит цифровую копию?" href="'+file.delete_url+'"><i class="icon-remove"></i></a></div>');
+	  '<a rel="nofollow" data-remote="true" data-method="delete" data-confirm="Удалить цифровую копию?" href="'+file.delete_url+'"><i class="icon-remove"></i></a></div>');
+      },
+      onChange: function (event) {
+        $('.pics_upload').data('filecount', 0);
       }
     });
 });
