0d711169ba62409f625c1fa51f152db298698968
Олег Шлыков <oshlykov@iteq.ru>
2012-03-07 22:19:23 +0400

    добавление индекс роута для экспорта

--- config/routes.rb
+++ config/routes.rb
@@ -110,8 +110,8 @@ Nkapp::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
+  match '/exchange' => "exchange#index"
   namespace :exchange do
-    get 'index'
     get 'export_prepare'
     get 'export'
   end
